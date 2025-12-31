9f09ed7b009a19697ef6d6d1ff06193d14fc0e0f
Mehrnazhi <mehrnaz.hosseini7596@gmail.com>
2025-12-31 15:11:58 +0400

    upload new files

--- sql_advance/problem_6.sql
+++ sql_advance/problem_6.sql
@@ -1,21 +1,21 @@
--- January
-CREATE TABLE january_job AS
+-- For January
+CREATE TABLE january_jobs AS
 SELECT *
 FROM job_postings_fact
 WHERE EXTRACT(
         MONTH
         FROM job_posted_date
     ) = 1;
--- February
-CREATE TABLE february_job AS
+-- For February
+CREATE TABLE february_jobs AS
 SELECT *
 FROM job_postings_fact
 WHERE EXTRACT(
         MONTH
         FROM job_posted_date
     ) = 2;
--- March
-CREATE TABLE march_job AS
+-- For March
+CREATE TABLE march_jobs AS
 SELECT *
 FROM job_postings_fact
 WHERE EXTRACT(
